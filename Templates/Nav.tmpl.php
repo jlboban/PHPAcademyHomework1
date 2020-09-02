1775da00df4e49e3e79aa5368eec6c36a22f603d
Jure Lucian Boban <jlboban17@gmail.com>
2020-09-02 16:02:47 +0200

    Initial commit

---
+++ Templates/Nav.tmpl.php
@@ -0,0 +1,38 @@
+<nav class="navbar is-danger">
+
+    <div class="navbar-brand">
+        <div class="navbar-item">
+            <h1 class="title is-4 ">PHP Academy HW 1</h1>
+        </div>
+        <div class="navbar-burger burger" data-target="navbar-basic">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <div id="navbar-basic" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="index.php">
+                Home
+            </a>
+            <a class="navbar-item" href="operators.php">
+                Operators
+            </a>
+            <a class="navbar-item" href="about.php">
+                About
+            </a>
+        </div>
+    </div>
+
+    <div class="navbar-end">
+        <div class="navbar-item">
+            <div class="buttons">
+                <a class="button is-dark" href="https://github.com/jlboban/PHPAcademyHomework1">
+                    <i class="fab fa-github"></i><strong class="has-padding-left">Github</strong>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</nav>
---
+++ operators.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<?php require_once 'Templates/Head.tmpl.php'; ?>
+<body>
+<?php require_once 'Templates/Nav.tmpl.php'; ?>
+<section class="section">
+    <main class="content">
+        <h1 class="title">
+            Operators
+        </h1>
+        <p class="subtitle">
+
+        </p>
+    </main>
+    <?php require_once 'Templates/Footer.tmpl.php' ?>
+</section>
+</body>
+</html>
